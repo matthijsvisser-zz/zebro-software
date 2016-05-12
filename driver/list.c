348aed67112534a9097e206f02d4c7b929eb0962
Matthijs Visser <matthijs.visser@hva.nl>
2016-05-12 17:18:09 +0200

    Population code toegevoegd

---
+++ driver/list.c
@@ -0,0 +1,310 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define DATASIZE 8
+#define FIRSTDATAITEM 0
+
+typedef struct node {
+	int data [DATASIZE];
+	struct node * next;
+}node_t;
+
+node_t *head = NULL;
+
+int sizeOfList(node_t *head);
+
+void printHeaderList (void){
+	printf("\nPopulationlist\n");
+    printf("Nr.\t");
+    for (int iteration = 0; iteration < DATASIZE; ++iteration){
+    	printf("Data[%d]\t", iteration);
+    }
+    printf("\n");
+}
+
+/**
+ * Print data from list
+ * @param  *head    List variable
+ * @param  item 	Used for iteration
+ * @param  *current Status pointer to the current memory location
+ * @param  data     Data from memory
+ * @param  next    Status pointer to the next memory location
+ */
+void print_list(node_t *head) {
+	int item, listNumber = 0;
+    node_t *current = head;
+
+    printHeaderList();
+    while(current != NULL) {
+    	printf("%d\t", listNumber);
+        for(item = 0; item < DATASIZE; item++){
+        	
+        	printf("%d\t",current ->  data[item]);
+        }  
+        printf("\n");
+        current = current -> next;
+        ++listNumber;
+    }
+}
+
+/**
+ * Appends a data array to the end of the list.
+ * @param  *head    List variable
+ * @param  data     Data to write to memory
+ * @param  *current Status pointer to the current memory location
+ * @param  next    Status pointer to the next memory location
+ */
+void append (node_t *head, int data[DATASIZE]){
+	node_t *current = head;
+	while (current -> next != NULL){
+		current = current -> next;
+	}
+	current -> next = (struct node *) malloc(sizeof(node_t));
+	for (int item = 0; item < DATASIZE; ++item){
+		current -> next -> data[item] = data[item];
+	}
+	current -> next -> next = NULL;
+}
+
+/**
+ * Inserts a data array to the beginning of the list.
+ * @param  *head    List variable
+ * @param  data     Data to write to memory
+ * @param  *current Status pointer to the current memory location
+ * @param  next    Status pointer to the next memory location
+ * @param  item 	Used for iteration
+ */
+void insert (node_t ** head, int data[DATASIZE]){
+	node_t * new_node;
+	new_node = (struct node *) malloc(sizeof(node_t));
+
+	for (int item = 0; item < DATASIZE; ++item){
+		new_node -> data[item] = data[item];
+	}
+	new_node -> next = *head;
+	*head = new_node;
+}
+
+/**
+ * Reads the first data array from the list and removes it afterwards.
+ * @param  *head    List variable
+ * @param  data     Data to write to memory
+ * @param  *current Status pointer to the current memory location
+ * @param  next     Status pointer to the next memory location
+ * @param  item 	Used for iteration
+ * @return returnArray Returns a pointer to the array where the data is stored. 
+ */
+int* pop (node_t ** head){
+    static int returnArray[DATASIZE];
+    node_t * next_node = NULL;
+
+    if (*head == NULL) {
+        return NULL;
+    }
+    next_node = (*head)->next;
+    for (int item = 0; item < DATASIZE; ++item){
+    	returnArray[item] = (*head) -> data[item];
+	}
+
+    free(*head);
+    *head = next_node;
+
+	return returnArray;
+}
+
+int* getListData (node_t **head){
+	node_t * next_node = NULL;
+	static int returnArray[DATASIZE];
+
+	next_node = (*head)->next;
+    for (int item = 0; item < DATASIZE; ++item){
+    	returnArray[item] = (*head) -> data[item];
+    	printf("%d\t", (*head) -> data[item]);
+	}
+	printf("\n");
+
+}
+
+void popList(){
+
+}
+
+/**
+ * Pops the data array with the given index number from the list and 
+ * removes it afterwards.
+ * @param  *head    List variable
+ * @param  indexNumber Index number of the wanted data array
+ * @param  data     Data to write to memory
+ * @param  *current Status pointer to the current memory location
+ * @param  next     Status pointer to the next memory location
+ * @param  item 	Used for iteration
+ * @return returnArray Returns a pointer to the array where the data is stored. 
+ */
+int* popListByNumber(node_t ** head, int indexNumber){
+	static int returnArray[DATASIZE];
+	node_t *current = *head, *temp_node = NULL;
+
+	if (indexNumber == 0){
+		return pop(head);
+	}
+
+	for (int item = 0; item < indexNumber-1; ++item){
+		if (current -> next == NULL){
+			return NULL;
+		}
+		current = current -> next;
+	}
+
+	temp_node = current -> next;
+
+	for (int item = 0; item < DATASIZE; ++item){
+    	returnArray[item] = temp_node -> data[item];
+	}
+	current -> next = temp_node -> next;
+	free(temp_node);
+
+
+	return returnArray;
+}
+
+/**
+ * Pops the data array with the given value at the first data array
+ * from the list andremoves it afterwards.
+ * @param  *head    List variable
+ * @param  indexNumber Index number of the wanted data array
+ * @param  data     Data to write to memory
+ * @param  *current Status pointer to the current memory location
+ * @param  next     Status pointer to the next memory location
+ * @param  item 	Used for iteration
+ * @return returnArray Returns a pointer to the array where the data is stored. 
+ */
+int* popListByValue(node_t ** head, int value, int sizeOfList){
+	static int returnArray[DATASIZE];
+	node_t *current = *head,*temp_node = NULL;
+	
+	// printf("Item %d, data [%d]\n", 0, current -> data[FIRSTDATAITEM]);
+
+	for (int item = 1; item < sizeOfList; ++item){
+		temp_node = current -> next;
+		// printf("Item %d, data [%d]\n", item, temp_node -> data[FIRSTDATAITEM]);
+		if(temp_node -> data[FIRSTDATAITEM] == value){
+			for (int item = 0; item < DATASIZE; ++item){
+    			returnArray[item] = temp_node -> data[item];
+			}
+		}
+		current = current -> next;
+	}
+	
+	current -> next = temp_node -> next;
+	free(temp_node);
+
+	return returnArray;
+}
+
+/**
+ * Retrieves the amount of data arrays in the list.
+ * @param  *head    List variable
+ * @param  *current Status pointer to the current memory location
+ * @param  next    Status pointer to the next memory location
+ * @param  sizeOfList Amount of data arrays in the list.
+ */
+int sizeOfList(node_t *head){
+	int sizeOfList = 1;
+	node_t * current = head;
+
+	while (current -> next != NULL){
+		current = current -> next;
+		sizeOfList++;
+	}
+	return sizeOfList;
+}
+
+int main(void){
+
+	int datb[DATASIZE];
+	
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+5;
+	}
+
+	insert(&head,datb);
+
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+1;
+	}
+
+	append(head,datb);
+
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+8;
+	}
+	append(head,datb);
+
+	print_list(head);
+
+	printf("POP\n");
+	printf("POP't information\n");
+	int* value = pop(&head);
+	if (pop != NULL){
+		for (int i = 0; i < DATASIZE; ++i){
+			printf("%d\t",*value);
+			*value++;
+		}
+	}
+	printf("\n");
+
+	printf("\n");
+	print_list(head);
+
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+15;
+	}
+
+	append(head,datb);
+
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+18;
+	}
+	append(head,datb);
+	printf("size of list:%d\n",sizeOfList(head));
+
+	print_list(head);
+
+	printf("POP't information\n");
+	value = popListByNumber(&head, 2);
+	if (pop != NULL){
+		for (int i = 0; i < DATASIZE; ++i){
+			printf("%d\t",*value);
+			*value++;
+		}
+	}
+	printf("\n");
+	print_list(head);
+
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+75;
+	}
+
+	append(head,datb);
+
+	for (int i = 0; i < DATASIZE; ++i){
+		datb[i]=i+98;
+	}
+	append(head,datb);
+	printf("size of list:%d\n",sizeOfList(head));
+
+	print_list(head);
+
+	printf("POP't information\n");
+	value = popListByValue(&head, 75, sizeOfList(head));
+	if (pop != NULL){
+		for (int i = 0; i < DATASIZE; ++i){
+			printf("%d\t",*value);
+			*value++;
+		}
+	}
+
+	print_list(head);
+
+	return 0;
+}
