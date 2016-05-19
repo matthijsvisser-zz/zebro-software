807564aa651a747563b0737e9a6f009107a2e5de
Matthijs Visser <matthijs.visser@hva.nl>
2016-05-19 11:26:01 +0200

    Header bestand toegevoegd

---
+++ driver/transceive.h
@@ -0,0 +1,17 @@
+/**
+ *	File	transceive.h
+ *	Created 19-05-2016
+ *	Author	Matthijs Visser
+ *			Amsterdam University of applied sciences
+ *	contact Matthijs.Visser@hva.nl
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Determines the meaning of the message
+ * @param  	value	received value
+ * @param	DetermineCommandtype
+ */
